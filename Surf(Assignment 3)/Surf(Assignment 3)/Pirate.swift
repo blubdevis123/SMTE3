c5448a609bfab4a3c7cd645009323c1953550523
blubdevis123 <fhict@MacBook-Pro.local>
2015-09-24 11:36:46 +0200

    fix assignment 2 startup assignment 3

---
+++ Surf(Assignment 3)/Surf(Assignment 3)/Pirate.swift
@@ -0,0 +1,12 @@
+//
+//  Pirate.swift
+//  Surf(Assignment 3)
+//
+//  Created by Fhict on 17/09/15.
+//  Copyright (c) 2015 Fhict. All rights reserved.
+//
+
+import Foundation
+class pirate {
+
+}
